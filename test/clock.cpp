580c5c776f52ffa3045c74e9fd028beed3908e4e
Quickeman <finnquicke@hotmail.com>
2021-10-05 12:58:36 +0100

    Added Clock tests to determine behaviour when starting a Clock that is already active with a different period

--- test/clock.cpp
+++ test/clock.cpp
@@ -6,17 +6,34 @@ using namespace std;
 using namespace std::chrono;
 
 int main() {
-    Clock clock;
+    Clock clock1;
     int clk_count = 0;
     test::check(clk_count, 0);
 
-    clock.start(milliseconds(50), [&clk_count](){ clk_count++; });
+    auto clocker = [&clk_count](){ clk_count++; };
+
+    clock1.start(milliseconds(20), clocker);
     test::check(clk_count, 0, 0, "Clock, just started");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(clk_count, 1, 0, "Clock, after 60ms");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(clk_count, 2, 0, "Clock, after 120ms");
-    clock.stop();
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(clk_count, 2, 0, "Clock, 180ms after start, 60ms after stop");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(clk_count, 1, 0, "Clock, after 22ms");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(clk_count, 2, 0, "Clock, after 44ms");
+    clock1.stop();
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(clk_count, 2, 0, "Clock, 66ms after start, 22ms after stop");
+
+    // Test re-starting with different period
+    Clock clock2;
+    clk_count = 0;
+    clock2.start(milliseconds(20), clocker);
+    test::check(clk_count, 0, 0, "Clock re-start, just started.");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(clk_count, 1, 0, "Clock re-start, after 22ms");
+    clock2.start(milliseconds(10), clocker);
+    test::check(clk_count, 1, 0, "Clock re-start, just after re-starting.");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(clk_count, 3, 0, "Clock re-start, after 22ms.");
+    clock2.stop();
+    std::this_thread::sleep_for(milliseconds(30));
+    test::check(clk_count, 3, 0, "Clock, 52ms after start, 30ms after stop");
 }
