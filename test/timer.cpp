7af8df5499f4a5ec3e22f10332937944b55b2c97
Quickeman <finnquicke@hotmail.com>
2021-10-05 12:59:01 +0100

    Tweaked Timer test to take less time

--- test/timer.cpp
+++ test/timer.cpp
@@ -22,35 +22,35 @@ int main() {
     TestCallback timer_cb;
     test::check(timer_cb.count, 0);
 
-    timer.start(milliseconds(50), timer_cb);
+    timer.start(milliseconds(20), timer_cb);
     test::check(timer_cb.count, 0, 0, "Timer, just started");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(timer_cb.count, 1, 0, "Timer, after 60ms");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(timer_cb.count, 1, 0, "Timer, after 120ms");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(timer_cb.count, 1, 0, "Timer, after 22ms");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(timer_cb.count, 1, 0, "Timer, after 44ms");
 
     // Test for re-using the Timer
-    timer.start(milliseconds(50), timer_cb);
+    timer.start(milliseconds(20), timer_cb);
     test::check(timer_cb.count, 1, 0, "Timer repeat, just started");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(timer_cb.count, 2, 0, "Timer repeat, after 60ms");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(timer_cb.count, 2, 0, "Timer repeat, after 120ms");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(timer_cb.count, 2, 0, "Timer repeat, after 22ms");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(timer_cb.count, 2, 0, "Timer repeat, after 44ms");
 
     // Test for re-using the Timer with stop()
-    timer.start(milliseconds(50), timer_cb);
+    timer.start(milliseconds(20), timer_cb);
     test::check(timer_cb.count, 2, 0, "Timer stop, just started");
-    std::this_thread::sleep_for(milliseconds(10));
+    std::this_thread::sleep_for(milliseconds(5));
     timer.stop();
-    test::check(timer_cb.count, 2, 0, "Timer stop, after 10ms");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(timer_cb.count, 2, 0, "Timer stop, after 70ms");
+    test::check(timer_cb.count, 2, 0, "Timer stop, after 5ms");
+    std::this_thread::sleep_for(milliseconds(25));
+    test::check(timer_cb.count, 2, 0, "Timer stop, after 30ms");
 
     // Test for re-using the Timer
-    timer.start(milliseconds(50), timer_cb);
+    timer.start(milliseconds(20), timer_cb);
     test::check(timer_cb.count, 2, 0, "Timer restarted, just started");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(timer_cb.count, 3, 0, "Timer restarted, after 60ms");
-    std::this_thread::sleep_for(milliseconds(60));
-    test::check(timer_cb.count, 3, 0, "Timer restarted, after 120ms");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(timer_cb.count, 3, 0, "Timer restarted, after 22ms");
+    std::this_thread::sleep_for(milliseconds(22));
+    test::check(timer_cb.count, 3, 0, "Timer restarted, after 44ms");
 }
